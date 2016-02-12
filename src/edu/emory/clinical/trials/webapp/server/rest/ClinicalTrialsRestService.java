a62bf3c66446cf0f2aee28ced9d714869ed405ee
daniel9x <dsgrant@emory.edu>
2016-02-12 14:20:21 -0500

    Committing

--- src/edu/emory/clinical/trials/webapp/server/rest/ClinicalTrialsRestService.java
+++ src/edu/emory/clinical/trials/webapp/server/rest/ClinicalTrialsRestService.java
@@ -347,6 +347,6 @@ public final class ClinicalTrialsRestService {
 	@Path("/test")
 	@Produces("text/html") 
 	public String getTestacular() {
-		return "<body bgcolor=\"#000000\"><center><div><link rel=\"stylesheet\" type=\"text/css\" href=\"../../style.css\"><div><span class='boring'><center>This is the ClinicalTrials test page. Yawn.</center></span></div>";
+		return "<center><div><link rel=\"stylesheet\" type=\"text/css\" href=\"../../style.css\"><div><span class='boring'><center>This is the ClinicalTrials test page. Yawn.</center></span></div>";
 	}
 }
